b0d3203e23037c514185cb52bb11a7565623c7d0
kincsolakatos <kincso.lakatos@stud.uni-corvinus.hu>
2024-11-30 13:26:38 +0100

    welcome modositva

--- WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net6.0/WebApplication1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2595a70a20ea85ea57e8fb007f653ae1a528e7cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
